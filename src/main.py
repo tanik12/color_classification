1c7f24bf0699a76dd9ebd4b0029ac96593b4cc44
tanik12 <koyu.tani01@g.softbank.co.jp>
2020-02-02 22:44:42 +0900

    [update]main関数から実行できるようにupdate

--- src/main.py
+++ src/main.py
@@ -1,44 +1,27 @@
 import cv2
 import numpy as np
+import glob
+import os
 
-# 赤色の検出
-def detect_red_color(img):
-    # HSV色空間に変換
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+from data_load import data_load, extract_color_info 
 
-    # 赤色のHSVの値域1
-    hsv_min = np.array([0,144,153])
-    hsv_max = np.array([8,194,252])
-    mask = cv2.inRange(hsv, hsv_min, hsv_max)
+def main():
+    label_dict = {"pedestrian_signs_blue":0, "pedestrian_signs_red":1, "vehicle_signal_blue":2, "vehicle_signal_red":3, "vehicle_signal_yellow":4}
 
-    # マスキング処理
-    masked_img = cv2.bitwise_and(img, img, mask=mask)
+    data_dir = "data/sample_trim/*"
+    current_dir = os.chdir('../')
+    current_dir = os.getcwd()
 
-    return mask, masked_img
+    target_pathes = os.path.join(current_dir, data_dir)
+    path_list = glob.glob(target_pathes)
 
-# 青色の検出
-def detect_blue_color(img):
-    # HSV色空間に変換
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    data_list = data_load(path_list, label_dict)
+    res_data = extract_color_info(data_list)
 
-    # 青色のHSVの値域1
-    hsv_min = np.array([84, 163, 108])
-    hsv_max = np.array([104, 203, 155])
-
-    # 青色領域のマスク（255：赤色、0：赤色以外）    
-    mask = cv2.inRange(hsv, hsv_min, hsv_max)
-
-    # マスキング処理
-    masked_img = cv2.bitwise_and(img, img, mask=mask)
-
-    return mask, masked_img
-
-def main(img_path):
-    img = cv2.imread(img_path)
-    red_mask, red_masked_img = detect_red_color(img)
-    cv2.imwrite("red_mask.png", red_mask)
-    cv2.imwrite("red_masked_img.png", red_masked_img)
+    #res_data -> [[赤色抽出した後のhsv, 青色抽出した後のhsv, 正解ラベル, 画像path], [...], ..., [...]]
+    print(res_data[0][1].shape)
+    cv2.imwrite("red_masked_img.png", res_data[0][0])
+    cv2.imwrite("bule_masked_img.png", res_data[0][1])
 
 if __name__ == "__main__":
-    data_path = '画像までのパスを書く'
-    main(data_path)
+    main()
